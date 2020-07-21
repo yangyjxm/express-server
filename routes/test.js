ab53e277e30e8be90e1c354c553e0af0ca688f0d
yangyjxm <yangyjxm@foxmail.com>
2020-07-21 17:45:32 +0800

    发送邮件功能

--- routes/test.js
+++ routes/test.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require('nodemailer');
 const axios = require('axios');
 const qs = require('qs');
 const CronJob = require('cron').CronJob;
@@ -11,18 +12,38 @@ router.get('', function (req, res, next) {
     title: '测试专用链接'
   });
   let i = 0
-  var AVenueJob = new CronJob('* * * * * *', function () {
-    console.log(i++)
-    if (i === 3) {
-      finish = true
-    }
-    if (finish) {
-      AVenueJob.stop()
-    }
-  }, null, true);
-
+  console.log(location.href)
+  // var AVenueJob = new CronJob('* * * * * *', function () {
+  //   console.log(i++)
+  //   sendMail()
+  //   if (i === 1) {
+  //     finish = true
+  //   }
+  //   if (finish) {
+  //     AVenueJob.stop()
+  //   }
+  // }, null, true);
 });
 
-function test() {}
+function sendMail() {
+  let transport = nodemailer.createTransport({
+    host: 'smtp.qq.com',
+    secureConnection: true,
+    port: 465,
+    auth: {
+      user: "yangyjxm@foxmail.com",
+      pass: "xdjjzcrkrzbubiai"
+    }
+  })
+  transport.sendMail({
+    from: '厦门市体育中心 yangyjxm@foxmail.com',
+    to: 'yangyjxm@foxmail.com',
+    subject: '球场预定成功',
+    text: '本周球场已经预定成功'
+  }, function (err, res) {
+    if (err) console.log(err)
+    else console.log(res)
+  })
+}
 
 module.exports = router;
